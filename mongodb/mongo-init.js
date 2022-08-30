bfa96ba37a00a64fbc7fabb746fddfdf003f5f58
Yasin TUNCEL <yasintuncel.eee@gmail.com>
2022-08-30 14:17:31 +0300

    mongo url fixed

---
+++ mongodb/mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: process.env.MONGODB_USER,
+        pwd: process.env.MONGODB_PASSWORD,
+        roles: [
+            {
+                role: "readWrite",
+                db: MONGODB_DATABASE,
+            }
+        ]
+    }
+);
--- server.js
+++ server.js
@@ -16,14 +16,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/users', router);
 app.options('*', cors());
 
+const {
+    MONGODB_USER,
+    MONGODB_PASSWORD,
+    MONGODB_HOST,
+    MONGODB_LOCAL_PORT,
+    MONGODB_DATABASE,
+} = process.env;
 
 const start = async function () {
     console.log("Starting server... ");
-    const dbStatus = await mongoConnector('mongodb://127.0.0.1:27017');
+    console.log(`mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_LOCAL_PORT}/${MONGODB_DATABASE}`);
+    const dbStatus = await mongoConnector(`mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_LOCAL_PORT}/${MONGODB_DATABASE}`);
     console.log(dbStatus.message);
 
     if (dbStatus.isConnected) {
-        app.listen(process.env.APP_PORT);
+        app.listen(process.env.NODE_DOCKER_PORT);
     } else {
         console.log("Exiting now!");
         process.exit(1);
